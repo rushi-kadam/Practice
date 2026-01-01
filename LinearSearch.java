85d628d4ced2bd87687b2ced4e9d29e327ab4b92
rushi-kadam <rushikeshgk899@gmail.com>
2026-01-01 09:38:25 +0530

    String search in linear search algorithm

--- LinearSearch.java
+++ LinearSearch.java
@@ -16,8 +16,17 @@ public class LinearSearch {
         return -1;
     }
 
+    // To search in string as string is an array of characters
+    static boolean linearSearch3(String str, char target){
+        if(str.length()==0) return false;
+        for(char ch: str.toCharArray()){
+            if(ch == target) return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         int[] numbers={1,2,3,4,5};
-        int found = linearSearch2(numbers,3);
+        String name = "Rushikesh";
+        boolean found = linearSearch3(name,'f');
         System.out.println(found);    }
 }
