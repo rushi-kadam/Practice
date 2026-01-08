a9d9e0ea285f6ad7927c3628a83e15a8a543b840
rushi-kadam <rushikeshgk899@gmail.com>
2026-01-08 21:58:10 +0530

    35.Search Insert Position

---
+++ BinarySearch/searchInsert.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/search-insert-position/
+package BinarySearch;
+
+public class searchInsert {
+      public int searchInsert(int[] nums, int target) {
+           int start=0;
+        int end= nums.length-1;
+        while(start<end){
+            // find mid
+            int mid= start +(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid] > target) end=mid-1;
+            else start=mid+1;
+        }
+       
+     return start;
+    }
+}
